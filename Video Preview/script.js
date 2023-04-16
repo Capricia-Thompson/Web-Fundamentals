c9945bb2ef7f2d9413d20c8a7cd1f3ae6b04e99c
Capricia-Thompson <dkcat192@gmail.com>
2023-04-16 08:06:12 -0400

    updates

--- Video Preview/script.js
+++ Video Preview/script.js
@@ -7,21 +7,6 @@ function startPreview() {
     video.play()
 }
 
-// function stopPreview() {
-//     video.pause()
-// }
-
-// const video = document.querySelector("video");
-
-// function startPreview() {
-//     // video.muted = true;
-//     video.currentTime = 1;
-//     video.playbackRate = 0.5;
-//     video.play();
-// }
-
 function stopPreview() {
-    video.currentTime = 0;
-    video.playbackRate = 1;
     video.pause();
 }
